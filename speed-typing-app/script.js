facb7d782f86a90fdd4d1f601e122bb08d30012a
roshni-salpekar <salpekarrosh@gmail.com>
2022-03-15 13:44:10 +0530

    Added random quote generating functionality

---
+++ speed-typing-app/script.js
@@ -0,0 +1,23 @@
+const RANDOM_QUOTE_API_URL = "https://api.quotable.io/random";
+const quoteDisplayElement = document.getElementById("quoteDisplay");
+const quoteInputElement = document.getElementById("quoteInput");
+
+function getRandomQuote() {
+  return fetch(RANDOM_QUOTE_API_URL)
+    .then((response) => response.json())
+    .then((data) => data.content)
+    .catch((err) => err);
+}
+
+async function renderNewQuote() {
+  const quote = await getRandomQuote();
+  quoteDisplayElement.innerHTML = "";
+  quote.split("").forEach((character) => {
+    const characterSpan = document.createElement("span");
+    characterSpan.innerText = character;
+    quoteDisplayElement.appendChild(characterSpan);
+  });
+  quoteInputElement.value = null;
+}
+
+renderNewQuote();
